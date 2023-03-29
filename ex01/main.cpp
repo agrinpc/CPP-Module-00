c636b310112cd7539e0d4304ddb0c2544814eda4
Milad Ahmadi soleimanieh <miahmadi@3-G-6.42heilbronn.de>
2023-03-29 15:03:41 +0200

    v 0.2.6

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -27,16 +27,42 @@ void	add_contact(Phonebook *phonebook)
 	phonebook->add(contact);
 }
 
+std::string	format_text(std::string s)
+{
+	s += "          ";
+	return (s.length() > 20 ? s.substr(0, 9) + "." : s.substr(0, 10));
+}
+
 void	search(Phonebook phonebook)
 {
 	int	size;
 	int	i = -1;
+	int	idx;
 	Contact *contacts;
 
 	size = phonebook.get_size();
 	contacts = phonebook.get_contacts();
 	while (++i < size)
-		std::cout << i + 1 << " - " << contacts[i].get_fname() <<  " " << contacts[i].get_lname() << " " << contacts[i].get_number() << " " << contacts[i].get_dark() << std::endl;
+	{
+		std::cout << i + 1 << "|";
+		std::cout << format_text(contacts[i].get_fname()) <<  "|";
+		std::cout << format_text(contacts[i].get_lname()) << "|";
+		std::cout << format_text(contacts[i].get_number()) << "|";
+		std::cout << format_text(contacts[i].get_dark()) << std::endl;
+	}
+	std::istringstream(get_input("Select Contact: ")) >> idx;
+	if (idx && idx < size + 1)
+		print_single(contacts[idx - 1]);
+	else
+		std::cout << "Index is not valid!" << std::endl;
+}
+
+void print_single(Contact contact)
+{
+	std::cout << "First Name:  " << contact.get_fname() << std::endl;
+	std::cout << "Last Name:   " << contact.get_lname() << std::endl;
+	std::cout << "Number:      " << contact.get_number() << std::endl;
+	std::cout << "Dark Secret: " << contact.get_dark() << std::endl;
 }
 
 void	run_cmd(Phonebook *phonebook)
--- ex01/main.h
+++ ex01/main.h
@@ -1,9 +1,11 @@
 #ifndef PHONEBOOK_H
 # define PHONEBOOK_H
 # include <iostream>
+# include <sstream>
 # include "./Contact.hpp"
 # include "./Phonebook.hpp"
 
 std::string get_input(std::string msg);
+void 		print_single(Contact contact);
 
 #endif
