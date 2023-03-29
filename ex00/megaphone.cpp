62e451343b0e434aeb84882c4e62865387ab80ac
Milad Ahmadi soleimanieh <miahmadi@3-G-6.42heilbronn.de>
2023-03-29 11:35:31 +0200

    v 0.2.1

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -3,10 +3,10 @@
 int	main(int argc, char **argv)
 {
 	int		i = 0;
-	int		j;
+	size_t	j;
 	char	*s;
 
-	if (argc == 1)
+	if (argc < 2)
 	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return (0);
